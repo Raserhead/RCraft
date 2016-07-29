b19e7aa30593fcf4674d23c76e9de03ac89296e1
Raserhead <raserheadd@gmail.com>
2016-07-30 04:25:31 +1000

    Added

---
+++ 0.4/res.bat
@@ -0,0 +1,33 @@
+@echo off
+title RaserCraft-1.8_Menu
+
+:choice
+cls
+echo (%time%) WARNING: Server closed or crashed.
+set /P a=do you want to restart? [Y/N]
+if /I "%a%" EQU "Y" goto :restart
+if /I "%a%" EQU "N" goto :choice2
+goto :choice
+
+:restart
+cls
+echo Restarting the server...
+timeout /t 10 /nobreak
+start init.bat
+exit
+
+:choice2
+set /P a=Launch config menu? [Y/N]
+if /I "%a%" EQU "Y" goto :configlaunch
+if /I "%a%" EQU "N" goto :stop
+goto :choice2
+
+:configlaunch
+start Start.bat
+exit
+
+:stop
+cls
+echo Closing the server...
+timeout /t 5 /nobreak
+exit
---
+++ 0.8/scripts/2GB.bat
@@ -0,0 +1,12 @@
+@echo off
+title RaserCraft-1.8
+setlocal
+set Directory=%~dp0
+set Directory=%Directory:~0,-1%
+for %%d in (%Directory%) do set ParentDirectory=%%~dpd
+cd %ParentDirectory%
+echo (%time%) Server Startup
+echo Enter command "stop" to shutdown the server.
+timeout /t 5 /nobreak
+start /B /wait java -server -XX:+UseConcMarkSweepGC -XX:OnOutOfMemoryError="kill -9 %p" -Xmx2G -jar spigot.jar nogui -o true
+exit
